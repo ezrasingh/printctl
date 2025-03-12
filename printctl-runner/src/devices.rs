179177a75c680b4d1797dff0e05c034750b09b27
Ezra Singh <singhezra@gmail.com>
2025-03-12 17:29:33 -0400

    Trying permisive string for device path

--- printctl-runner/src/devices.rs
+++ printctl-runner/src/devices.rs
@@ -3,14 +3,14 @@ use std::path::PathBuf;
 
 #[derive(Debug)]
 pub struct Device {
-    path: PathBuf,
+    path: String,
     vendor: String,
     product: String,
     usb: String,
 }
 
 impl Device {
-    pub fn path(&self) -> &PathBuf {
+    pub fn path(&self) -> &String {
         &self.path
     }
     pub fn vendor(&self) -> &String {
@@ -24,13 +24,7 @@ impl Device {
     }
 
     pub fn port(&self) -> Result<SystemPort, serial::Error> {
-        serial::open(self.path.as_path())
-    }
-}
-
-impl From<Device> for PathBuf {
-    fn from(value: Device) -> Self {
-        value.path
+        serial::open(&self.path)
     }
 }
 
@@ -40,7 +34,7 @@ impl From<SerialInfo> for Device {
     fn from(serial_info: SerialInfo) -> Self {
         let field_or_else = || Some(String::from("--"));
         return Self {
-            path: serial_info.name.into(),
+            path: serial_info.name,
             vendor: serial_info.vendor.or_else(field_or_else).unwrap(),
             product: serial_info.product.or_else(field_or_else).unwrap(),
             usb: serial_info
