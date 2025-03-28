920ca4a228d4cdac8775a09c0e096f097af29bc6
Ezra Singh <singhezra@gmail.com>
2025-03-27 21:58:26 -0400

    Updated config

--- printctl/src/cli.rs
+++ printctl/src/cli.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use serde::Deserialize;
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -13,34 +14,48 @@ pub enum Command {
     /// Start user interface
     Ui {
         /// Run web interface
-        #[arg(long, action)]
-        web: bool,
+        #[arg(short = 'w', long = "web", action)]
+        use_web: bool,
 
-        #[arg(short, long, env = "PRINTCTL_UI_PORT")]
-        port: Option<u16>,
-    },
-    /// List available connections
-    List {
-        /// List connected devices
-        #[arg(short, long, action)]
-        device: bool,
-
-        /// List connected nodes
-        #[arg(short, long, action)]
-        node: bool,
-    },
-    /// Start printctl node
-    Node {
-        /// The address the server will bind to
-        #[arg(short, long, env = "PRINTCTL_NODE_ADDR")]
+        /// The address the web server will bind to
+        #[arg(short, long, env = "PRINTCTL_WEB_UI_ADDR")]
         addr: Option<std::net::IpAddr>,
 
-        /// The port the server will listen on
-        #[arg(short, long, env = "PRINTCTL_NODE_PORT")]
+        /// The port the web server will listen on
+        #[arg(short, long, env = "PRINTCTL_WEB_UI_PORT")]
         port: Option<u16>,
 
         /// Specify a config file
         #[arg(short, long = "config", env = "PRINTCTL_CONFIG")]
         config_path: Option<PathBuf>,
     },
+    /// List available connections
+    List {
+        /// List connected devices
+        #[arg(short, long, action)]
+        devices: bool,
+
+        /// List connected machines
+        #[arg(short, long, action)]
+        machines: bool,
+    },
+    /// Start printctl server
+    Start {
+        /// The address the server will bind to
+        #[arg(short, long, env = "PRINTCTL_GRPC_ADDR")]
+        addr: Option<std::net::IpAddr>,
+
+        /// The port the server will listen on
+        #[arg(short, long, env = "PRINTCTL_GRPC_PORT")]
+        port: Option<u16>,
+
+        /// Specify a config file
+        #[arg(short, long = "config", env = "PRINTCTL_CONFIG")]
+        config_path: Option<PathBuf>,
+    },
+}
+
+#[derive(Debug, Default, Deserialize)]
+pub struct PrintctlConfig {
+    pub server: Option<printctl_node::server::ServerConfig>,
 }
--- printctl/src/command.rs
+++ printctl/src/command.rs
@@ -20,20 +20,25 @@ impl CommandRunner {
         let cluster_node =
             printctl_node::mesh::Node::<Idle>::new(gethostname().into_string().unwrap(), &None)?;
         match self.0 {
-            cli::Command::Ui { web, port } => {
-                if web {
+            cli::Command::Ui {
+                use_web,
+                addr,
+                port,
+                config_path,
+            } => {
+                if use_web {
                     dashboard::web::start(port.unwrap_or(8080))
                         .expect("could not start web user interface");
                 } else {
                     dashboard::tui::start().expect("could not start terminal user interface");
                 }
             }
-            cli::Command::List { device, node } => {
+            cli::Command::List { devices, machines } => {
                 let active_node = cluster_node.start_discovery();
                 for _ in 0..20 {
                     println!("Listening for peers...");
                     let peers = active_node.peers().await;
-                    if node {
+                    if machines {
                         for peer in peers {
                             println!("{:#?}", peer);
                         }
@@ -43,7 +48,7 @@ impl CommandRunner {
                 println!("Ending peer discovery...");
                 active_node.stop_discovery().await;
             }
-            cli::Command::Node {
+            cli::Command::Start {
                 addr,
                 port,
                 config_path,
