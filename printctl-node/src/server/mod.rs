4876d9d1057df3110cb418934ade86f06ca68b4d
Ezra Singh <singhezra@gmail.com>
2025-03-30 07:03:56 -0400

    Improved tonic entrypoint

--- printctl-node/src/server/mod.rs
+++ printctl-node/src/server/mod.rs
@@ -1,5 +1,4 @@
 mod config;
-
 pub use config::ServerConfig;
 
 #[path = "./"]
@@ -16,19 +15,24 @@ use crate::prelude::*;
 use simple_mdns::async_discovery::ServiceDiscovery;
 use tonic::transport::Server;
 
-use crate::mesh;
+use crate::discovery;
 
-impl grpc::Printctl for mesh::Node<ServiceDiscovery> {}
+#[tonic::async_trait]
+impl grpc::Printctl for discovery::Node<ServiceDiscovery> {}
 
 #[tokio::main]
 pub async fn run(config: ServerConfig) -> Result<()> {
-    let addr = "[::1]:50051".parse().unwrap();
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 
-    println!("Advertising machine as: {}", config.node_name());
-    let node = mesh::Node::new(config.node_name(), &None)?.start_discovery();
+    tracing::info!(message = "Stating discovery...");
+    let node_name = config.node_name();
+    let node = discovery::Node::new(node_name, &None)?.start_discovery();
+    tracing::info!(message = "Advertising self as: ", %node_name);
 
-    println!("PrintctlServer listening on {}", addr);
-
+    let addr = config.grpc_socket();
+    tracing::info!(message = "gRPC server listening on {}", %addr);
     Server::builder()
         .add_service(grpc::PrintctlServer::new(node))
         .serve(addr)
