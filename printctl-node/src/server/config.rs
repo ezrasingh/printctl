7b6b81350fe69ec2686eb008834104f6ceea0892
Ezra Singh <singhezra@gmail.com>
2025-03-30 07:03:21 -0400

    Removed options

--- printctl-node/src/server/config.rs
+++ printctl-node/src/server/config.rs
@@ -1,34 +1,38 @@
 use serde::Deserialize;
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct ServerConfig {
     node_name: String,
-    service_address: Option<IpAddr>,
-    grpc_socket: Option<std::net::SocketAddr>,
+    grpc_address: IpAddr,
+    grpc_port: u16,
 }
 
 impl ServerConfig {
     pub fn new(
         node_name: impl Into<String>,
-        service_address: Option<IpAddr>,
-        grpc_socket: Option<std::net::SocketAddr>,
+        grpc_address: Option<IpAddr>,
+        grpc_port: Option<u16>,
     ) -> Self {
         Self {
             node_name: node_name.into(),
-            service_address,
-            grpc_socket,
+            grpc_address: grpc_address.unwrap_or(Ipv4Addr::new(0, 0, 0, 0).into()),
+            grpc_port: grpc_port.unwrap_or(50051),
         }
     }
     pub fn node_name(&self) -> &str {
         &self.node_name
     }
 
-    pub fn service_address(&self) -> &Option<IpAddr> {
-        &self.service_address
+    pub fn grpc_address(&self) -> &IpAddr {
+        &self.grpc_address
     }
 
-    pub fn grpc_socket(&self) -> &Option<std::net::SocketAddr> {
-        &self.grpc_socket
+    pub fn grpc_port(&self) -> &u16 {
+        &self.grpc_port
+    }
+
+    pub fn grpc_socket(&self) -> SocketAddr {
+        SocketAddr::new(*self.grpc_address(), *self.grpc_port())
     }
 }
